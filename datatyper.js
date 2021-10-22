10336485fb67a12ffd9d530440640f350e4462c7
elliot.thuren <elliot.thuren@elev.ga.ntig.se>
2021-10-22 10:48:39 +0200

    🧪

---
+++ datatyper.js
@@ -0,0 +1,9 @@
+let name = "Farmor";
+let num = 12;
+num = prompt('Hur många burkar dunderhonung?');
+let num1 = 12, num2 = 12;
+let sum = num1 + num2;
+console.log(`Summan blir ${sum}`);
+sum = num + num1;
+console.log('Den nya summan blir: ' + sum);
+sum = parseInt(num) + num1;
